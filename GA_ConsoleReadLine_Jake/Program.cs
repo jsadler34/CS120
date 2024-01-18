136b8b99dfae5ffb8e65a0ac0d2bc77336f80371
jsadler@student.rtc.edu <jsadler@stuent.rtc.edu>
2024-01-17 21:57:32 -0800

    Commiting ConsoleReadLine assignment

---
+++ GA_ConsoleReadLine_Jake/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GA_ConsoleReadLine_Jake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            // Accept User Input
+            String username = Console.ReadLine();
+            // Replay User Input
+            Console.WriteLine("Hello " + username + "!");
+            // Ask for Favorite Color
+            Console.WriteLine("What is your favorite color?");
+            // Get Response
+            string color = Console.ReadLine();
+            // Respond
+            Console.WriteLine(color + " is a great color, " + username);
+        }
+    }
+}
