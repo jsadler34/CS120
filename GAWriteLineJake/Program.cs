75ad01d701f45f0fc6b96bc9c95fd333a402cf10
jsadler@student.rtc.edu <jsadler@stuent.rtc.edu>
2024-01-17 21:21:11 -0800

    Adding Program

---
+++ GAWriteLineJake/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GAWriteLineJake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Edgar W. Dijkstra:");
+            Console.WriteLine("Computer science is no more about computers than astronomy is about telescopes.");
+
+            Console.Write("Alan Kay: ");
+            Console.WriteLine("The best way to predict the future is to invent it.");
+
+            Console.Write("Grace Hopper: ");
+            Console.WriteLine("The most damaging phrase in the language is, We’ve always done it this way.");
+
+            Console.Write("Terry Pratchett: ");
+            Console.WriteLine("It's not worth doing something unless someone, somewhere, would much rather you weren't doing it.");
+        }
+    }
+}
