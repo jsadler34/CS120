e0b615979e86b4077961b18b31acb0948daa80b5
jsadler@student.rtc.edu <jsadler@stuent.rtc.edu>
2024-01-17 21:48:47 -0800

    Trying to commit the program again.

---
+++ GAStrings_Jake/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GAStrings_Jake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myfirstname = "Jake";
+            string mylastname = "Sadler";
+            string myfullname = myfirstname + " " + mylastname;
+            Console.WriteLine(myfullname);
+
+            //Reassigning my profs name
+            myfirstname = "William";
+            mylastname = "Cram";
+            myfullname = myfirstname + " " + mylastname + " is my professor";
+            Console.WriteLine(myfullname);
+        }
+    }
+} // End of Main
